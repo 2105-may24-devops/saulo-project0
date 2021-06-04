afce430897aefbf40395fbf8d044726e62fef728
sadelarosa <sadelarosa@ufl.edu>
2021-06-04 10:00:43 -0400

    added some more functionality to main

--- main.py
+++ main.py
@@ -1,62 +1,123 @@
 import sys
-import pathlib
+import base64
+import os
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import kdf
+from cryptography.hazmat.primitives.kdf import KeyDerivationFunction
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from pathlib import *
 
-#current_path = os.path.abspath('.')
-#parent_path = os.path.dirname(current_path)
-#sys.path.append(parent_path)
-
-def checkParams():
-    if len(sys.argv) == 3:
+def checkParams(numbArgs,firstArg):
+    if numbArgs == 3:
         return True
+    elif numbArgs == 2 and  firstArg == "-h":
+        print("<REPLACE ME> with a print out of the instructions on how to use this program and all the flag options.")
+        return False
     else:
-        print("Incorrect parameters. Correct ussage: main.py <-flag> <file name or path to file>")
+        print("Incorrect arguments. Correct ussage: py main.py <-flag> <file name or path to file>")
         return False
 
-def encrypt():
+def passwordRules():
+    password = input("Enter password: ")
+
+    while len(password) < 8:
+        print("Passwords must be atleast 8 characters long. Please try again.")
+        password = input("Enter password: ")
+
+    confirmPass = input("Enter password again to confirm: ") 
+
+    while password != confirmPass:
+        print("Passwords did not match. Please try again.")
+        password = input("Enter password: ")
+
+        while len(password) < 8:
+            print("Passwords must be atleast 8 characters long. Please try again.")
+            password = input("Enter password: ")
+
+        confirmPass = input("Enter password again to confirm: ") 
+
+    return password
+
+
+def aesEncrypt(filePath):
+    pass
+
+def fernetEncrypt(filePath):
+    password = passwordRules()
+    baseSalt = b'\xbf\xe2\xd1\xaf\xbc\xb1\xdd\x82\xe2\xaf\xbc\xdd\x27\xd9\x82\xbf\x61\x62'
+    userSalt = input("Enter salt value:")
+    userSalt = str.encode(userSalt)
+    finalSalt = userSalt + baseSalt
+    kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=finalSalt,
+            iterations=200000,
+            )
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+    f = Fernet(key)
+    token = f.encrypt(b"Secret message!")
+
+def nsaEncrypt(filePath):
+    pass
+
+def encrypt(filePath):
+    encryptionType = input("How would you like your file to be encrypted? (options are: aes256, fernet, nsa)")
+
+    if(encryptionType == "aes256"):
+        aesEncrypt(filePath)
+
+    if(encryptionType == "fernet"):
+        fernetEncrypt(filePath)
+
+    if(encryptionType == "fernet"):
+        nsaEncrypt(filePath)
+
+
+def decrypt(filePath):
     pass
 
 
-def decrypt():
+def sendFile(filePath):
     pass
 
 
-def sendFile():
-    pass
-
-
-def changePass():
+def changePass(filePath):
     pass
 
 
 def helpInfo():
     pass
 
-def recieveFile():
+def recieveFile(filePath):
     pass
 
-def firstArgument(flag):
+def destroyFile(filePath):
+    pass
+
+def arguments(flag, filePath):
     if( flag == "-e"):
-        encrypt()
+        encrypt(filePath)
     if( flag == "-d"):
-        decrypt()
+        decrypt(filePath)
     if( flag == "-s"):
-        sendFile()
+        sendFile(filePath)
     if( flag == "-c"):
-        changePass()
+        changePass(filePath)
     if( flag == "-h"):
         helpInfo()
+    if( flag == "-destroy"):
+        destroyFile(filePath)
     if( flag == "-r"):
-        recieveFile()
-
-
-#from functions.py import checkParams
+        recieveFile(filePath)
 
 def main():
-    if checkParams():
+    if checkParams(len(sys.argv), sys.argv[1]):
         print ("correct inputs")
-        firstArgument(sys.argv[1])
+        arguments(sys.argv[1], sys.argv[2])
     else:
-        print ("incorrect inputs")
+        pass
 
 
 if __name__ == "__main__":
