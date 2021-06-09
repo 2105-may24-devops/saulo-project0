61a425034b9a2ba30412543a345315dcedb4a1e3
sadelarosa <sadelarosa@ufl.edu>
2021-06-09 12:55:49 +0000

    created test_script.sh for testing

--- test_script.sh
+++ test_script.sh
@@ -4,9 +4,17 @@
 
    main () {
 
-   	cat test/inputs_enc.txt | python3 -m main.py -e testfile1.txt
+   	cat test/inputs_enc.txt | python3 main.py -e test/file1.txt
 
- 
+	if cmp test/enc_results.txt test/new.txt; then
+		echo "Encrypted file matches the expected result." >> test/test_results.txt
+
+   	else
+   		echo "ERROR: Encrypted file does NOT matches the expected result." > test/test_results.txt
+                exit 1
+
+	fi
+	exit 0
 }
 
 
