847f04481bafc31aa9b39754465490b77dc24b46
sadelarosa <sadelarosa@ufl.edu>
2021-06-10 19:39:39 -0400

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -6,20 +6,35 @@
 	
 	echo "" > test/test_results.txt
 	
-   	cat test/inputs_enc.txt | python3 main.py -e test/file1.txt
-
-	if cmp test/enc_results.txt test/new_enc.txt; then
-		echo "Encrypted file matches the expected result." >> test/test_results.txt
-		echo "Encrypted file matches the expected result."
+   	cat test/inputs_enc.txt | python3 main.py -e test/file1.txt > /dev/null
+	cat test/inputs_dec.txt | python3 main.py -d test/new_enc.txt > /dev/null
+	
+	if cmp test/file1.txt test/new_enc.txt; then
+		echo "Encrypted then derypted file matches the expected result." >> test/test_results.txt
+		echo "Encrypted then derypted file matches the expected result."
 
    	else
-   		echo "ERROR: Encrypted file does NOT matches the expected result." >> test/test_results.txt
-		echo "ERROR: Encrypted file does NOT matches the expected result."
+   		echo "ERROR: Encrypted then derypted file does NOT matches the expected result." >> test/test_results.txt
+		echo "ERROR: Encrypted then derypted file does NOT matches the expected result." 
                 exit 1
 
 	fi
 	
-	cat test/inputs_dec.txt | python3 main.py -d test/new_enc.txt
+	cat test/inputs_enc.txt | python3 main.py -e test/example.txt > /dev/null
+	cat test/inputs_dec.txt | python3 main.py -d test/new_enc.txt > /dev/null
+
+	if cmp test/example.txt test/new_enc.txt; then
+		echo "Encrypted then derypted file matches the expected result." >> test/test_results.txt
+		echo "Encrypted then derypted file matches the expected result."
+
+   	else
+   		echo "ERROR: Encrypted then derypted file does NOT matches the expected result." >> test/test_results.txt
+		echo "ERROR: Encrypted then derypted file does NOT matches the expected result."
+                exit 1
+
+	fi
+	
+	
 	
 	if cmp test/file1.txt test/new_enc.txt; then
 		echo "derypted file matches the expected result." >> test/test_results.txt
