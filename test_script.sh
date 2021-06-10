292e869986de7ba652635277287cbd8682f548b2
sadelarosa <sadelarosa@ufl.edu>
2021-06-10 19:45:57 -0400

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -10,12 +10,12 @@
 	cat test/inputs_dec.txt | python3 main.py -d test/new_enc.txt > /dev/null
 	
 	if cmp test/file1.txt test/new_enc.txt; then
-		echo "Encrypted then derypted file matches the expected result." >> test/test_results.txt
-		echo "Encrypted then derypted file matches the expected result."
+		echo "Encrypted then decrypted test/file1.txt matches the expected result." >> test/test_results.txt
+		echo "Encrypted then decrypted test/file1.txt matches the expected result."
 
    	else
-   		echo "ERROR: Encrypted then derypted file does NOT matches the expected result." >> test/test_results.txt
-		echo "ERROR: Encrypted then derypted file does NOT matches the expected result." 
+   		echo "ERROR: Encrypted then decrypted test/file1.txt does NOT matches the expected result." >> test/test_results.txt
+		echo "ERROR: Encrypted then decrypted test/file1.txtt does NOT matches the expected result." 
                 exit 1
 
 	fi
@@ -24,12 +24,12 @@
 	cat test/inputs_dec.txt | python3 main.py -d test/new_enc.txt > /dev/null
 
 	if cmp test/example.txt test/new_enc.txt; then
-		echo "Encrypted then derypted file matches the expected result." >> test/test_results.txt
-		echo "Encrypted then derypted file matches the expected result."
+		echo "Encrypted then decrypted test/example.txt matches the expected result." >> test/test_results.txt
+		echo "Encrypted then decrypted test/example.txt matches the expected result."
 
    	else
-   		echo "ERROR: Encrypted then derypted file does NOT matches the expected result." >> test/test_results.txt
-		echo "ERROR: Encrypted then derypted file does NOT matches the expected result."
+   		echo "ERROR: Encrypted then decrypted test/example.txt does NOT matches the expected result." >> test/test_results.txt
+		echo "ERROR: Encrypted then decrypted test/example.txt does NOT matches the expected result."
                 exit 1
 
 	fi
