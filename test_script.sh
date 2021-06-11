3c2939b260c55bec6dd0cba5c604774ecd322cc7
sadelarosa <sadelarosa@ufl.edu>
2021-06-11 11:08:25 -0400

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -8,12 +8,12 @@
 	cat test/inputs_dec.txt | python3 main.py -d test/new_enc.txt > /dev/null
 	
 	if cmp test/file1.txt test/new_enc.txt; then
-		echo "Encrypted then decrypted test/file1.txt matches the expected result." >> test/test_results.txt
-		echo "Encrypted then decrypted test/file1.txt matches the expected result."
+		echo "(interactive)Encrypted then decrypted test/file1.txt matches the expected result." >> test/test_results.txt
+		echo "(interactive)Encrypted then decrypted test/file1.txt matches the expected result."
 
    	else
-   		echo "ERROR: Encrypted then decrypted test/file1.txt does NOT matches the expected result." >> test/test_results.txt
-		echo "ERROR: Encrypted then decrypted test/file1.txtt does NOT matches the expected result." 
+   		echo "ERROR: (interactive)Encrypted then decrypted test/file1.txt does NOT matches the expected result." >> test/test_results.txt
+		echo "ERROR: (interactive)Encrypted then decrypted test/file1.txtt does NOT matches the expected result." 
                 exit 1
 
 	fi
@@ -22,12 +22,12 @@
 	cat test/inputs_dec.txt | python3 main.py -d test/new_enc.txt > /dev/null
 
 	if cmp test/example.txt test/new_enc.txt; then
-		echo "Encrypted then decrypted test/example.txt matches the expected result." >> test/test_results.txt
-		echo "Encrypted then decrypted test/example.txt matches the expected result."
+		echo "(interactive)Encrypted then decrypted test/example.txt matches the expected result." >> test/test_results.txt
+		echo "(interactive)Encrypted then decrypted test/example.txt matches the expected result."
 
    	else
-   		echo "ERROR: Encrypted then decrypted test/example.txt does NOT matches the expected result." >> test/test_results.txt
-		echo "ERROR: Encrypted then decrypted test/example.txt does NOT matches the expected result."
+   		echo "ERROR: (interactive)Encrypted then decrypted test/example.txt does NOT matches the expected result." >> test/test_results.txt
+		echo "ERROR: (interactive))Encrypted then decrypted test/example.txt does NOT matches the expected result."
                 exit 1
 
 	fi
@@ -36,12 +36,12 @@
 	python3 main.py -d test/example.txt fernet password salt > /dev/null
 	
 	if cmp test/example.txt test/new_enc.txt; then
-		echo "Encrypted then decrypted test/example.txt matches the expected result." >> test/test_results.txt
-		echo "Encrypted then decrypted test/example.txt matches the expected result."
+		echo "(noninteractive)Encrypted then decrypted test/example.txt matches the expected result." >> test/test_results.txt
+		echo "(noninteractive)Encrypted then decrypted test/example.txt matches the expected result."
 
    	else
-   		echo "ERROR: Encrypted then decrypted test/example.txt does NOT matches the expected result." >> test/test_results.txt
-		echo "ERROR: Encrypted then decrypted test/example.txt does NOT matches the expected result."
+   		echo "ERROR: (noninteractive)Encrypted then decrypted test/example.txt does NOT matches the expected result." >> test/test_results.txt
+		echo "ERROR: (noninteractive)Encrypted then decrypted test/example.txt does NOT matches the expected result."
                 exit 1
 
 	fi
