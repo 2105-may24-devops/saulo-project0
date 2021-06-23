5e054df4c5f99611e19122443cb3aa910597256c
sadelarosa <sadelarosa@ufl.edu>
2021-06-23 08:02:27 -0400

    Update aes256.py

--- aes256.py
+++ aes256.py
@@ -1,12 +1,21 @@
 # AES 256 encryption/decryption using pycrypto library
 import base64
+from types import DynamicClassAttribute
 from Crypto.Cipher import AES
 from Crypto import Random
 from Crypto.Protocol.KDF import PBKDF2
  
-BLOCK_SIZE = 16
-pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
-unpad = lambda s: s[:-ord(s[len(s) - 1:])]
+
+
+def extend(data , length_of_blocks):
+    extended = length_of_blocks - len(data) % length_of_blocks
+    extended = extended * chr(length_of_blocks - len(data) % length_of_blocks)
+    extended = data + extended
+    return extended
+
+def unextend(data):
+    unextended =  data[:-ord(data[len(data) - 1:])]
+    return unextended
  
 password = input("Enter encryption password: ")
  
@@ -19,9 +28,10 @@ def get_private_key(password):
  
  
 def encrypt(raw, password):
+    length_of_blocks = 16
     private_key = get_private_key(password)
-    raw = pad(raw)
-    iv = Random.new().read(AES.block_size)
+    raw = extend(raw, length_of_blocks)
+    iv = Random.new().read(AES.length_of_block)
     cipher = AES.new(private_key, AES.MODE_CBC, iv)
     return base64.b64encode(iv + cipher.encrypt(raw))
  
@@ -31,7 +41,7 @@ def decrypt(enc, password):
     enc = base64.b64decode(enc)
     iv = enc[:16]
     cipher = AES.new(private_key, AES.MODE_CBC, iv)
-    return unpad(cipher.decrypt(enc[16:]))
+    return unextend(cipher.decrypt(enc[16:]))
  
  
 # First let us encrypt secret message
