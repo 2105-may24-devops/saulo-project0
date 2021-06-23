91fb103840a2b8c178a0916d01c9b916856b01d8
sadelarosa <sadelarosa@ufl.edu>
2021-06-23 08:58:31 -0400

    Update aes256.py

--- aes256.py
+++ aes256.py
@@ -5,7 +5,7 @@ from Crypto.Cipher import AES
 from Crypto import Random
 from Crypto.Protocol.KDF import PBKDF2
  
-
+password = input("Enter encryption password: ")
 
 def extend(data , length_of_blocks):
     extended = length_of_blocks - len(data) % length_of_blocks
@@ -17,31 +17,38 @@ def unextend(data):
     unextended =  data[:-ord(data[len(data) - 1:])]
     return unextended
  
-password = input("Enter encryption password: ")
- 
- 
-def get_private_key(password):
-    salt = b"this is a salt"
-    kdf = PBKDF2(password, salt, 64, 1000)
-    key = kdf[:32]
-    return key
- 
  
 def encrypt(raw, password):
     length_of_blocks = 16
-    private_key = get_private_key(password)
-    raw = extend(raw, length_of_blocks)
+#    password = input("Enter encryption password: ")
+    salt = b"this is a salt."
+    kdf = PBKDF2(password, salt, 64, 1000)
+    key = kdf[:32]
+    private_key =key
+
+    data = raw
+    extended = length_of_blocks - len(data) % length_of_blocks
+    extended = extended * chr(length_of_blocks - len(data) % length_of_blocks)
+    extended = data + extended
+
     iv = Random.new().read(AES.block_size)
     cipher = AES.new(private_key, AES.MODE_CBC, iv)
-    return base64.b64encode(iv + cipher.encrypt(raw))
+    return base64.b64encode(iv + cipher.encrypt(extended))
  
  
 def decrypt(enc, password):
-    private_key = get_private_key(password)
+    length_of_blocks = 16
+ #   password = input("Enter encryption password: ")
+    salt = b"this is a salt."
+    kdf = PBKDF2(password, salt, 64, 1000)
+    key = kdf[:32]
+    private_key = key
     enc = base64.b64decode(enc)
     iv = enc[:16]
     cipher = AES.new(private_key, AES.MODE_CBC, iv)
-    return unextend(cipher.decrypt(enc[16:]))
+    data = cipher.decrypt(enc[16:])
+    unextended =  data[:-ord(data[len(data) - 1:])]
+    return unextended
  
  
 # First let us encrypt secret message
